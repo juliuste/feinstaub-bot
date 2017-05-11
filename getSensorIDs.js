90bb871a287956e8a93d07cf79985546039f283b
Julius Te <mail@juliuste.de>
2017-05-11 15:06:35 +0200

    add regional key(s) config option

--- getSensorIDs.js
+++ getSensorIDs.js
@@ -1,21 +1,65 @@
 'use strict'
 
+const config = require('config')
 const fetch = require('node-fetch')
 const filter = require('lodash.filter')
-const berlin = require('german-states-bbox').BE
+const includes = require('lodash.includes')
+const inside = require('@turf/inside')
+const turf = require('@turf/helpers')
+const fs = require('fs-extra')
+const flatten = require('geojson-flatten')
 
-const isInBBox = (coords, bbox) => (
-		(coords.latitude <= bbox[0])
-	&&	(coords.latitude >= bbox[2])
-	&&	(coords.longitude <= bbox[3])
-	&&	(coords.longitude >= bbox[1])
-)
+const generateFileName = () => (config.regions.join('-') || 'empty') + '.json'
 
-const getSensorIDs = () =>
+const generateAreaList = () => {
+	const germany = require('german-administrative-areas')
+
+	const entities = []
+	for(let level in germany){
+		for(let element of germany[level]){
+			if(element && element.type==='Feature') entities.push(element)
+			if(element && element.type==='FeatureCollection') entities.push(...element.features)
+		}
+	}
+
+	const list = []
+	list.push(...filter(
+		entities,
+		(o) => (o.properties && includes(config.regions, o.properties.RS_0))
+	))
+	return list
+}
+
+const loadAreaList = () =>
+	fs.readJson(generateFileName())
+	.catch((err) => {
+		const areaList = generateAreaList()
+		fs.writeJson(generateFileName(), areaList)
+		return areaList
+	})
+
+const inArea = (coords, areas) => areas.some((area) => inside(coords, area))
+
+const getSensorDump = () =>
 	fetch('https://api.luftdaten.info/static/v2/data.dust.min.json')
 	.then((res) => res.json())
-	.then((res) => filter(res, (o) => isInBBox(o.location, berlin)))
-	.then((res) => res.map((o) => o.sensor.id))
-	.catch((err) => [])
+
+const getSensorIDs = () => {
+	if(!Array.isArray(config.regions) || config.regions.length === 0){
+		return Promise.reject(new Error('pass an array of one or more regions'))
+	}
+
+	return Promise.all([
+		loadAreaList(),
+		getSensorDump()
+	])
+	.then(([areaList, sensorIDs]) =>
+		filter(sensorIDs, (s) => inArea(
+			turf.point([+s.location.longitude, +s.location.latitude]),
+			areaList
+		))
+		.map((o) => o.sensor.id)
+	)
+}
 
 module.exports = getSensorIDs
--- readme.md
+++ readme.md
@@ -6,6 +6,45 @@
 
 [@berlinFeinstaub](https://twitter.com/berlinFeinstaub). Twitter bot that monitors fine dust pollution in Berlin using [luftdaten.info](http://luftdaten.info) community sensors. Tweets when PM10 emissions surpass 50 µg/m³. See also [further information about fine dust pollution in Germany](http://www.umweltbundesamt.de/en/topics/air/particulate-matter-pm10) provided by the German federal environmental protection agency. Inspired by [@FeinstaubFR](https://twitter.com/FeinstaubFR).
 
+## Configuration
+
+If you want to adapt the bot to watch your municipalities sensors instead, you can simply do so by changing the config file/s:
+
+### `config/default.json`
+
+```js
+{
+	"twitter": { // your Twitter API credentials **
+		"key": "X",
+		"key_secret": "X",
+		"token": "X",
+		"token_secret": "X"
+	},
+	"threshold": 50, // PM10 limit in µg/m³
+	"interval": 60, // interval for the bot to run in minutes
+	"regions": [ // list of german "Gemeindeschlüssel" keys *
+		"110000000000"
+	]
+}
+```
+
+\* If you only know your municipalities 7-digit key, just add '0000' for the 11-digit key. Example (Berlin): 11000000 -> 110000000000. You can give multiple municipality keys.
+
+** Even though you can add your Twitter API credentials in `default.json`, it's strongly recommended to store them in a seperate file called `production.json` which should look like this:
+
+```js
+{
+	"twitter": { // your Twitter API credentials **
+		"key": "X",
+		"key_secret": "X",
+		"token": "X",
+		"token_secret": "X"
+	}
+}
+```
+
+**If you want to run the bot using `production.json`, just execute `npm run start`.**
+
 ## Contributing
 
 If you found a bug, want to propose a feature or feel the urge to complain about your life, feel free to visit [the issues page](https://github.com/juliuste/berlinFeinstaub-bot/issues).
